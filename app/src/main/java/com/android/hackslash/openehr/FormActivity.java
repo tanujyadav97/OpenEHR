a0827881bab63bcb0668ce849417cf0abcd30357
Tanuj <tanujyadav171997@gmail.com>
2019-04-16 03:29:41 +0530

    Added Particular doctor features

--- app/src/main/java/com/android/hackslash/openehr/FormActivity.java
+++ app/src/main/java/com/android/hackslash/openehr/FormActivity.java
@@ -618,8 +618,39 @@ public class FormActivity extends AppCompatActivity {
         Map<String, String> fieldsmap = getMapfromList();
         String ts = ((Long) System.currentTimeMillis()).toString();
 
-        mFirestore.collection("EHR").document(patientUserid).collection(filename)
-                .document(ts).set(fieldsmap).addOnSuccessListener(new OnSuccessListener<Void>() {
+//        mFirestore.collection("EHR").document(patientUserid).collection(filename)
+//                .document(ts).set(fieldsmap).addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void aVoid) {
+//                Toast.makeText(getApplicationContext(), "Database Updated Successfully", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext(), "Unable to update database.", Toast.LENGTH_SHORT).show();
+//                        Log.e("FormActivity", e.toString());
+//                    }
+//                });
+
+        mFirestore.collection("EHR1").document("EHR1").collection(patientUserid)
+                .document(filename + '@' + ts).set(fieldsmap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Database Updated Successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Unable to update database.", Toast.LENGTH_SHORT).show();
+                        Log.e("FormActivity", e.toString());
+                    }
+                });
+        mFirestore.collection("EHR2").document("EHR2").collection(filename)
+                .document(patientUserid + '@' + ts).set(fieldsmap).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 Toast.makeText(getApplicationContext(), "Database Updated Successfully", Toast.LENGTH_SHORT).show();
